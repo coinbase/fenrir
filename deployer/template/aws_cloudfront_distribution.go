b145d49eb76dcb111e68fde8aa0233cd5458f603
Frank Hamand <frank.hamand@coinbase.com>
2019-07-25 14:46:11 +0100

    Disable S3 origins for cloudfront
    We can add the validations for these when we need them

--- deployer/template/aws_cloudfront_distribution.go
+++ deployer/template/aws_cloudfront_distribution.go
@@ -16,5 +16,12 @@ func ValidateAWSCloudFrontDistribution(
 	res.Tags = append(res.Tags, resources.Tag{Key: "ConfigName", Value: configName})
 	res.Tags = append(res.Tags, resources.Tag{Key: "ServiceName", Value: resourceName})
 
+	// Disallow s3 origins for now - we need to validate them securely which isn't trivial
+	for _, origin := range res.DistributionConfig.Origins {
+		if origin.S3OriginConfig != nil {
+			return resourceError(res, resourceName, "S3 Origins are not yet supported for cloudfront distributions")
+		}
+	}
+
 	return nil
 }
